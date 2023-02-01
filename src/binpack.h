24cb7fd80efe4e6b2b0dd320c79b62f055e3d372
Pedro Henrique Ferreira Zoz <pedrozoz.sizaan@gmail.com>
2023-02-01 08:42:49 -0300

    file_structure

---
+++ src/binpack.h
@@ -0,0 +1,13 @@
+#include "boxstructure.h"
+#include <vector>
+using namespace std;
+
+vector<BoxList> create_neighbors() // Leo
+{
+
+}
+
+void local_search(BoxList& box_list) // Luiz
+{
+
+}
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "boxstructure.h"
+#include "binpack.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
