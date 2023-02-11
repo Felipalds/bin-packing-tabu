007b691a7510a5113be2007f49aa1b371ec2c935
Luiz Felipe Fonseca Rosa <luizfelipefonsecarosa@gmail.com>
2023-02-11 15:12:59 -0300

    local search

--- src/BoxList.py
+++ src/BoxList.py
@@ -1,14 +1,55 @@
 class BoxList:
     boxes = [] #Box array
     fitness = int()
-    box_weight = int()
+    boxWeight = int()
 
-    def __init__(self, boxArray):
+    def __init__(self, boxArray, boxWeight):
         self.boxes = boxArray
+        self.boxWeight = boxWeight
         self.getFitness()
 
-    def generateLocalSearch(self) -> list("BoxList"):
-        pass
+    def generateLocalSearch(self) -> "BoxList":
+        currentList = self.boxes
+        newListLenght = len(currentList)
+        newList = currentList
+        for boxPosition, box in enumerate(currentList):
+            if(boxPosition >= 1):
+                if(len(newList[boxPosition - 1]) <= 1):
+                    newList.pop(0)
+                    newListLenght -= 1
+            isAdded = False
+            for itemPosition, item in enumerate(box):
+                if(isAdded):
+                    newList[boxPosition].pop(0)
+
+                isAdded = False
+                index = 0
+                while(not isAdded):
+                    index += 1
+                    position = newListLenght - index
+                    if(position < boxPosition):
+                        break
+                    if(self.getBoxWeight(position, newList) + item <= self.boxWeight):
+                        isAdded = True
+                        newList[position].append(item)
+                        break
+
+        return(newList)
+
+    def getBoxWeight(self, index, passedList):
+        if(index >= len(passedList)):
+            index -= 1
+        currentBox = passedList[index]
+        currentWeight = 0
+        for item in currentBox:
+            currentWeight += item
+        return currentWeight
+    
+    def generateEmptyList(self, length):
+        newList = []
+        for c in range(length):
+            newList.append([])
+        return newList
 
     def findNeighbors(self) -> list("BoxList"):
         pass
--- src/localSearch.test.py
+++ src/localSearch.test.py
@@ -1,7 +1,16 @@
 from BoxList import BoxList
 
 def __main__():
-    boxes = BoxList([1, 2, 3, 4, 5])
-    print(boxes.boxes)
+    currentBoxes = BoxList([[2],[2],[1],[2],[3],[3],[4]], 6)
+    localSearchArray = currentBoxes.generateLocalSearch()
+    print(localSearchArray)
+    
+    currentBoxes2 = BoxList([[1],[2],[3, 2],[3, 2],[4]], 6)
+    localSearchArray = currentBoxes2.generateLocalSearch()
+    print(localSearchArray)
+
+    currentBoxes3 = BoxList([[2],[1],[2],[3, 2],[3],[4]], 6)
+    localSearchArray = currentBoxes3.generateLocalSearch()
+    print(localSearchArray)
 
 __main__()
