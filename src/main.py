5ca9baa4b618cfa72febd43ae6fa0c6bcce59e83
pfzoz <pedrozoz.sizaan@gmail.com>
2023-02-12 13:34:09 -0300

    complete-flow-and-read

--- src/main.py
+++ src/main.py
@@ -19,18 +19,22 @@ with open(file_name, 'r') as file_handle:
     print(f"Tabu List: {solutions.tabu_list}\n")
     while (step < max_steps):
         print(f"-- NEIGHBORS --")
-        neighbors = BoxList.findNeighbors(box_list)
+        neighbors = BoxList.findNeighbors(solutions.current_list)
         for neighbor in neighbors: print(neighbor)
         opt_neighbors = [neighbor.generateLocalSearch() for neighbor in neighbors]
         print(f"-- NEIGHBORS (LS) --")
         for neighbor in opt_neighbors: print(neighbor)
-        solutions.local_solution = opt_neighbors[0]
-        for neighbor in opt_neighbors[1:]:
-            if not neighbor in solutions.tabu_list:
+        for neighbor in opt_neighbors:
+            if not neighbor in solutions.tabu_list.keys():
+                solutions.local_solution = neighbor
+                break
+        for neighbor in opt_neighbors:
+            if not neighbor in solutions.tabu_list.keys():
                 if neighbor.fitness >= solutions.global_solution.fitness:
                     solutions.global_solution = neighbor
-                if neighbor.fitness >= solutions.local_solution.fitness:
+                if neighbor.fitness > solutions.local_solution.fitness:
                     solutions.local_solution = neighbor
+                    solutions.current_list = neighbor
                 solutions.advance_tabu()
                 solutions.add_tabu(solutions.local_solution, tabu_count)
         print(f"== STEP {step} ==")
